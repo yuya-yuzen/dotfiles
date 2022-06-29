b49dfac7d6321b8828429ad76321338b38d7eb13
yuya <blue_sky_sora8@yahoo.co.jp>
2022-06-30 08:40:42 +0900

    add config/nvim

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,45 @@
+local options = {
+  number = true,
+
+  cursorline = true,
+  cursorcolumn = true,
+
+  list = true,
+
+  encoding = "utf-8",
+  fileencoding = "utf-8",
+
+  swapfile = false,
+  backup = false,
+  writebackup = false,
+  undofile = true,
+
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  smartindent = true,
+
+  showtabline = 2,
+
+  clipboard = "unnamedplus",
+
+  mouse = "a",
+
+  splitbelow = true,
+  splitright = true,
+
+  completeopt = { "menuone", "noinsert"},
+
+  conceallevel = 0,
+
+  wrap = false,
+
+  scrolloff = 8,
+  sidescrolloff = 8,
+
+  guifont = "Ricty:h18",
+}
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
+end
