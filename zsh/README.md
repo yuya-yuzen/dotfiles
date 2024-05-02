a7f4852e2206674050e1dcbba898e7e9d003069b
miyazono.yuya <miyazono.yuya@vega-c.com>
2024-05-02 17:15:44 +0900

    zshのalias_settingsを追加

---
+++ zsh/README.md
@@ -0,0 +1,5 @@
+# Zsh
+
+```
+source ~/path/to/alias_settings.zsh
+```
---
+++ zsh/alias_settings.zsh
@@ -0,0 +1,28 @@
+# ---
+# Git
+# ---
+
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+alias gpl='git pull'
+alias gc='git commit -m'
+alias gcan='git commit --amend --no-edit'
+alias gsw='git switch'
+alias gswc='git switch -c'
+alias gswm='git switch master'
+alias grm='git rebase master'
+alias gri='git rebase -i'
+
+# -----
+# Rails
+# -----
+
+alias br='bin/rspec'
+alias rA='rubocop -A'
+
+# ------
+# Others
+# ------
+
+alias ls='eza'
+alias cat='bat'
